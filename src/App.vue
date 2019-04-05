42ad2cc4b60c8c0c12c823eb23a8f7eb36c2646f
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2019-04-04 22:25:27 -0400

    Some styles on nav.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="flex h-screen overflow-hidden">
-    <div id="main-nav" class="w-48 overflow-y-auto">
-      <ul>
-        <li v-for="route in $router.options.routes" :key="route.name">
-          <router-link :to="route">{{ route.name }}</router-link>
+    <div id="main-nav" class="w-56 overflow-y-auto">
+      <ul class="nav-list">
+        <li
+          v-for="route in $router.options.routes"
+          :key="route.name"
+        >
+          <router-link class="nav-item" 
+          :class="{'active': route.path===$router.currentRoute.path}"
+          :to="route">{{ route.name }}</router-link>
         </li>
       </ul>
 
