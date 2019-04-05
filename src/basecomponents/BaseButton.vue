0304e1cf8693d44d205405836088e02b3191c916
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2019-04-05 10:49:21 -0400

    Made fill property for transparent instead.

--- src/basecomponents/BaseButton.vue
+++ src/basecomponents/BaseButton.vue
@@ -1,15 +1,3 @@
-<template>
-  <button
-    :class="allClass"
-    :disabled="disabled"
-    :type="type"
-    v-on="this.$listeners"
-    ref="btn"
-  >
-    <slot></slot>
-  </button>
-</template>
-
 <script>
 export default {
   props: {
@@ -18,9 +6,14 @@ export default {
       default: 'none',
       validator: function(value) {
         return (
-          ['none', 'transparent', 'success', 'warning', 'danger', 'info', 'primary'].indexOf(
-            value
-          ) !== -1
+          [
+            'none',
+            'success',
+            'warning',
+            'danger',
+            'info',
+            'primary'
+          ].indexOf(value) !== -1
         )
       }
     },
@@ -35,7 +28,13 @@ export default {
       type: String,
       default: 'button'
     },
-    plain: Boolean,
+    fill: {
+      type: String,
+      default: 'plain',
+      validator: function(value) {
+        return ['plain', 'full', 'none'].indexOf(value) !== -1
+      }
+    },
     rounded: Boolean,
     disabled: Boolean
   },
@@ -43,45 +42,55 @@ export default {
     allClass() {
       return {
         // defaults
-        'b-btn flex self-center items-center border': true,
+        'b-btn flex self-center items-center border font-semibold': true,
         // sizes
         'py-1 px-2 text-xs': this.size === 'xs',
         'py-1 px-3 text-sm': this.size === 'sm',
-        'py-2 px-4': this.size === 'md',
-        'py-3 px-5 text-lg': this.size === 'lg',
-
-        // colors
+        'py-2 px-6': this.size === 'md',
+        'py-3 px-6 text-lg': this.size === 'lg',
+        // full
         'border-gray-600 bg-gray-600 hover:bg-gray-700 text-gray-100':
-          this.color === 'info' && !this.plain,
+          this.color === 'info' && this.fill === 'full',
         'border-blue-500 bg-blue-600 hover:bg-blue-700 text-blue-100':
-          this.color === 'primary' && !this.plain,
+          this.color === 'primary' && this.fill === 'full',
         'border-green-500 bg-green-600 hover:bg-green-700 text-green-100':
-          this.color === 'success' && !this.plain,
+          this.color === 'success' && this.fill === 'full',
         'border-yellow-500 bg-yellow-700 hover:bg-yellow-800 text-yellow-100':
-          this.color === 'warning' && !this.plain,
+          this.color === 'warning' && this.fill === 'full',
         'border-red-500 bg-red-600 hover:bg-red-700 text-red-100':
-          this.color === 'danger' && !this.plain,
+          this.color === 'danger' && this.fill === 'full',
         'border-gray-500 bg-gray-100 hover:bg-gray-300': this.color === 'none',
-        'border-transparent hover:bg-gray-200': this.color === 'transparent',
-
-        // plains
-        'border-gray-600 bg-gray-100 hover:bg-gray-500 hover:text-white text-gray-700':
-          this.color === 'info' && this.plain,
-        'border-blue-500 bg-blue-100 hover:bg-blue-500 hover:text-white text-blue-700':
-          this.color === 'primary' && this.plain,
-        'border-green-500 bg-green-100 hover:bg-green-500 hover:text-white text-green-700':
-          this.color === 'success' && this.plain,
-        'border-yellow-500 bg-yellow-100 hover:bg-yellow-600 hover:text-white text-yellow-800':
-          this.color === 'warning' && this.plain,
-        'border-red-500 bg-red-100 hover:bg-red-500 hover:text-white text-red-700':
-          this.color === 'danger' && this.plain,
-
+        // plain
+        'border-gray-600 bg-gray-100 hover:bg-gray-700 hover:text-gray-100 text-gray-700':
+          this.color === 'info' && this.fill === 'plain',
+        'border-blue-700 bg-blue-100 hover:bg-blue-700 hover:text-blue-100 text-blue-700':
+          this.color === 'primary' && this.fill === 'plain',
+        'border-green-700 bg-green-100 hover:bg-green-700 hover:text-green-100 text-green-700':
+          this.color === 'success' && this.fill === 'plain',
+        'border-yellow-800 bg-yellow-100 hover:bg-yellow-800 hover:text-yellow-100 text-yellow-800':
+          this.color === 'warning' && this.fill === 'plain',
+        'border-red-700 bg-red-100 hover:bg-red-700 hover:text-red-100 text-red-700':
+          this.color === 'danger' && this.fill === 'plain',
+        // none
+        'border-transparent hover:text-gray-500 text-gray-700':
+          this.color === 'info' && this.fill === 'none',
+        'border-transparent hover:text-blue-500 text-blue-700':
+          this.color === 'primary' && this.fill === 'none',
+        'border-transparent hover:text-green-500 text-green-700':
+          this.color === 'success' && this.fill === 'none',
+        'border-transparent hover:text-yellow-500 text-yellow-800':
+          this.color === 'warning' && this.fill === 'none',
+        'border-transparent hover:text-red-500 text-red-700':
+          this.color === 'danger' && this.fill === 'none',
         // rounding
         'rounded-sm': !this.rounded,
         'rounded-full p-0': this.rounded,
         // interact
         'opacity-75 cursor-not-allowed': this.disabled
       }
+    },
+    isAnchor() {
+      return this.type === 'anchor'
     }
   },
   methods: {
@@ -89,6 +98,22 @@ export default {
       const btn = this.$refs.btn
       if (btn) btn.focus()
     }
+  },
+  render(h) {
+    return h(
+      this.isAnchor ? 'a' : 'button',
+      {
+        class: this.allClass,
+        attrs: {
+          ...this.$attrs,
+          disabled: this.disabled,
+          type: this.isAnchor ? undefined : this.type
+        },
+        on: this.$listeners,
+        ref: 'btn'
+      },
+      this.$slots.default
+    )
   }
 }
 </script>
@@ -98,7 +123,7 @@ export default {
   padding-right: .7em!important;
 } */
 .b-btn {
-  transition: background-color .2s;
+  transition: background-color 0.2s;
 }
 .b-btn + .b-btn {
   @apply ml-2;
