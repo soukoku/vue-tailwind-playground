44560d0f2e2a970aa646cf5fde74f00778d30680
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2019-04-06 11:28:39 -0400

    Fixed disabled button color.

--- src/components/TWButton.vue
+++ src/components/TWButton.vue
@@ -38,8 +38,17 @@ export default {
     disabled: Boolean
   },
   computed: {
+    fillFull() {
+      return this.fill === 'full'
+    },
+    fillPlain() {
+      return this.fill === 'plain'
+    },
+    fillNone() {
+      return this.fill === 'none'
+    },
     allClass() {
-      return {
+      const cls = {
         // defaults
         'tw-btn flex self-center items-center border': true,
         // sizes
@@ -47,47 +56,91 @@ export default {
         'py-1 px-4 text-sm': this.size === 'sm',
         'py-2 px-6': this.size === 'md',
         'py-3 px-6 text-lg': this.size === 'lg',
-        // full
-        'border-gray-600 bg-gray-600 hover:bg-gray-700 text-gray-100':
-          this.color === 'info' && this.fill === 'full',
-        'border-blue-500 bg-blue-600 hover:bg-blue-700 text-blue-100':
-          this.color === 'primary' && this.fill === 'full',
-        'border-green-500 bg-green-600 hover:bg-green-700 text-green-100':
-          this.color === 'success' && this.fill === 'full',
-        'border-yellow-500 bg-yellow-700 hover:bg-yellow-800 text-yellow-100':
-          this.color === 'warning' && this.fill === 'full',
-        'border-red-500 bg-red-600 hover:bg-red-700 text-red-100':
-          this.color === 'danger' && this.fill === 'full',
-        'border-gray-400 bg-white hover:bg-gray-300 text-gray-700':
-          this.color === 'none',
-        // plain
-        'border-gray-600 bg-gray-100 hover:bg-gray-700 hover:text-gray-100 text-gray-700':
-          this.color === 'info' && this.fill === 'plain',
-        'border-blue-700 bg-blue-100 hover:bg-blue-700 hover:text-blue-100 text-blue-700':
-          this.color === 'primary' && this.fill === 'plain',
-        'border-green-700 bg-green-100 hover:bg-green-700 hover:text-green-100 text-green-700':
-          this.color === 'success' && this.fill === 'plain',
-        'border-yellow-800 bg-yellow-100 hover:bg-yellow-800 hover:text-yellow-100 text-yellow-800':
-          this.color === 'warning' && this.fill === 'plain',
-        'border-red-700 bg-red-100 hover:bg-red-700 hover:text-red-100 text-red-700':
-          this.color === 'danger' && this.fill === 'plain',
-        // none
-        'border-transparent hover:text-gray-500 text-gray-700':
-          this.color === 'info' && this.fill === 'none',
-        'border-transparent hover:text-blue-500 text-blue-700':
-          this.color === 'primary' && this.fill === 'none',
-        'border-transparent hover:text-green-500 text-green-700':
-          this.color === 'success' && this.fill === 'none',
-        'border-transparent hover:text-yellow-500 text-yellow-800':
-          this.color === 'warning' && this.fill === 'none',
-        'border-transparent hover:text-red-500 text-red-700':
-          this.color === 'danger' && this.fill === 'none',
+
         // rounding
         'rounded-sm': !this.rounded,
         'rounded-full p-0': this.rounded,
         // interact
         'opacity-75 cursor-not-allowed': this.disabled
       }
+
+      if (this.color === 'info') {
+        const prop = this.fillFull
+          ? `border-gray-600 bg-gray-600 text-gray-100 ${
+              this.disabled ? '' : 'hover:bg-gray-700'
+            }`
+          : this.fillPlain
+          ? `border-gray-600 bg-gray-100 text-gray-700 ${
+              this.disabled ? '' : 'hover:bg-gray-700 hover:text-gray-100'
+            }`
+          : `border-transparent text-gray-700 ${
+              this.disabled ? '' : 'hover:text-gray-500'
+            }`
+        cls[prop] = true
+      } else if (this.color === 'primary') {
+        const prop = this.fillFull
+          ? `border-blue-500 bg-blue-600 text-blue-100 ${
+              this.disabled ? '' : 'hover:bg-blue-700'
+            }`
+          : this.fillPlain
+          ? `border-blue-700 bg-blue-100 text-blue-700 ${
+              this.disabled ? '' : 'hover:bg-blue-700 hover:text-blue-100'
+            }`
+          : `border-transparent text-blue-700 ${
+              this.disabled ? '' : 'hover:text-blue-500'
+            }`
+        cls[prop] = true
+      } else if (this.color === 'success') {
+        const prop = this.fillFull
+          ? `border-green-500 bg-green-600 text-green-100 ${
+              this.disabled ? '' : 'hover:bg-green-700'
+            }`
+          : this.fillPlain
+          ? `border-green-700 bg-green-100 text-green-700 ${
+              this.disabled ? '' : 'hover:bg-green-700 hover:text-green-100'
+            }`
+          : `border-transparent text-green-700 ${
+              this.disabled ? '' : 'hover:text-green-500'
+            }`
+        cls[prop] = true
+      } else if (this.color === 'warning') {
+        const prop = this.fillFull
+          ? `border-yellow-500 bg-yellow-600 text-yellow-100 ${
+              this.disabled ? '' : 'hover:bg-yellow-800'
+            }`
+          : this.fillPlain
+          ? `border-yellow-800 bg-yellow-100 text-yellow-800 ${
+              this.disabled ? '' : 'hover:bg-yellow-800 hover:text-yellow-100'
+            }`
+          : `border-transparent text-yellow-800 ${
+              this.disabled ? '' : 'hover:text-yellow-500'
+            }`
+        cls[prop] = true
+      } else if (this.color === 'danger') {
+        const prop = this.fillFull
+          ? `border-red-500 bg-red-600 text-red-100 ${
+              this.disabled ? '' : 'hover:bg-red-700'
+            }`
+          : this.fillPlain
+          ? `border-red-700 bg-red-100 text-red-700 ${
+              this.disabled ? '' : 'hover:bg-red-700 hover:text-red-100'
+            }`
+          : `border-transparent text-red-700 ${
+              this.disabled ? '' : 'hover:text-red-500'
+            }`
+        cls[prop] = true
+      } else {
+        const prop = this.fillNone
+          ? `border-transparent text-gray-700 ${
+              this.disabled ? '' : 'hover:text-gray-500'
+            }`
+          : `border-gray-400 bg-white text-gray-700 ${
+              this.disabled ? '' : 'hover:bg-gray-300'
+            }`
+        cls[prop] = true
+      }
+
+      return cls
     },
     allAttrs() {
       return {
