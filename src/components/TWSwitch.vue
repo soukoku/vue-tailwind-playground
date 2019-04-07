3353641b6843347c761c09e4f34c2a68eaeb1988
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2019-04-07 16:59:39 -0400

    Added switch sample.

---
+++ src/components/TWSwitch.vue
@@ -0,0 +1,77 @@
+<template>
+  <span
+    :class="{
+      'border-blue-600 bg-blue-600': checkedReal,
+      'border-gray-400': !checkedReal,
+      'opacity-50': disabled
+    }"
+    class="inline-flex rounded-full border bg-gray-400 cursor-pointer w-10"
+    @keydown.space="doToggle"
+    @click.prevent="doToggle"
+    :tabindex="disabled ? -1 : 0"
+    role="switch"
+    :aria-checked="checkedReal ? 'true' : 'false'"
+  >
+    <input type="checkbox" class="hidden" v-bind="allAttrs" />
+    <span
+      :class="{
+        'on border-blue-600': checkedReal,
+        'border-gray-400': !checkedReal
+      }"
+      class="switch-knob border rounded-full w-4 h-4 bg-white"
+    >
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  model: {
+    prop: 'checked',
+    event: 'change'
+  },
+  props: {
+    checked: Boolean,
+    disabled: Boolean
+  },
+  data() {
+    return {
+      checkedReal: this.checked
+    }
+  },
+  watch: {
+    checked(val) {
+      this.checkedReal = val
+    },
+    checkedReal(val) {
+      this.$emit('change', val)
+    }
+  },
+  computed: {
+    allAttrs() {
+      return {
+        ...this.$attrs,
+        disabled: this.disabled,
+        checked: this.checkedReal
+      }
+    }
+  },
+  methods: {
+    doToggle(e) {
+      if (!this.disabled) {
+        this.checkedReal = !this.checkedReal
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.switch-knob {
+  transition: transform 200ms;
+}
+.switch-knob.on {
+  transform: translateX(140%);
+}
+</style>
---
+++ src/pages/Switch.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" v-model="disabled" />
+      Toggle disabled
+    </label>
+    <br />
+
+    <tw-switch id="switch" v-model="checked" :disabled="disabled"></tw-switch>
+    <br />
+
+    <tw-switch id="switch" v-model="checked" :disabled="disabled"></tw-switch>
+    <label for="switch">
+      For label
+    </label>
+    <br />
+    <label>
+      <tw-switch :checked="checked" :disabled="disabled"></tw-switch>
+      Wrapped label
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      disabled: false,
+      checked: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
