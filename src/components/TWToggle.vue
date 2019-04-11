e7d3075a694a2dc6ff65a04abe633bdeaa327ac9
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2019-04-10 22:01:20 -0400

    Tweaked checkbox style.

--- src/components/TWToggle.vue
+++ src/components/TWToggle.vue
@@ -18,12 +18,13 @@
   <span
     v-else
     :class="{
-      'text-blue-600': (isChecked || indeterReal) && !this.disabled,
-      'text-gray-600': !isChecked || this.disabled,
+      'border-blue-600 text-blue-600':
+        (isChecked || indeterReal) && !this.disabled,
+      'border-gray-600': !isChecked || this.disabled,
       'opacity-50 cursor-not-allowed': disabled,
       'cursor-pointer': !disabled
     }"
-    class="inline-flex"
+    class="tw-check inline-flex flex-grow-0 items-center justify-center border rounded-sm overflow-hidden"
     @keydown.space="doToggle"
     @click.prevent="doToggle"
     :tabindex="disabled ? -1 : 0"
@@ -31,30 +32,26 @@
     :aria-checked="isChecked ? 'true' : 'false'"
   >
     <input type="checkbox" class="hidden" v-bind="allAttrs" />
+
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="w-5 h-5 fill-current"
       viewBox="0 0 24 24"
       xmlns:xlink="http://www.w3.org/1999/xlink"
+      class="tw-check-icon fill-current"
     >
-      <path
-        class="tw-check-icon opacity-0"
-        :class="{ 'opacity-100': !isChecked }"
-        d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"
-      />
-      <path
-        class="tw-check-icon opacity-0"
-        :class="{ 'opacity-100': isChecked }"
-        d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
-      />
-
-      <defs><path id="a" d="M0 0h24v24H0z" /></defs>
-      <clipPath id="b"><use xlink:href="#a" overflow="visible" /></clipPath>
-      <path
-        class="tw-check-icon opacity-0"
+      <rect
+        shape-rendering="crispEdges"
+        class="opacity-0"
         :class="{ 'opacity-100': indeterReal }"
-        clip-path="url(#b)"
-        d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2z"
+        width="18"
+        height="4"
+        x="3"
+        y="11"
+      ></rect>
+      <path
+        class="opacity-0"
+        :class="{ 'opacity-100': isChecked }"
+        d="M9,20.42L2.79,14.21L5.62,11.38L9,14.77L18.88,4.88L21.71,7.71L9,20.42Z"
       />
     </svg>
   </span>
@@ -141,13 +138,21 @@ export default {
 }
 </script>
 <style>
+.tw-check {
+  width: 20px;
+  height: 20px;
+}
 .tw-check-icon {
+  width: 18px;
+  height: 18px;
+}
+.tw-check-icon > * {
   transition: opacity 0.2s;
 }
 
 .tw-switch {
-  width: 44px;
-  height: 22px;
+  width: 40px;
+  height: 20px;
   transition: 0.2s;
 }
 
@@ -156,8 +161,8 @@ export default {
   content: '';
   top: 2px;
   left: 2px;
-  width: 18px;
-  height: 18px;
+  width: 16px;
+  height: 16px;
   transition: 0.3s;
 }
 
@@ -170,6 +175,6 @@ export default {
   width: 60%;
 }
 .tw-switch.disabled:active:after {
-  width: 18px;
+  width: 16px;
 }
 </style>
